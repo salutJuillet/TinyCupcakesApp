de229b4b49a83a2122d3b8e962c99464347383a3
salutJuillet <salut.juilllet@gmail.com>
2022-08-11 14:14:07 +0900

    파일 정리

--- App.js
+++ App.js
@@ -1,14 +1,10 @@
 import React from 'react'
 import { ScrollView } from 'react-native'
-// import ToggleMode from './components/ToggleMode'
-// import Style02 from './components/Style02'
 import Main from './components/Main'
 
 const App = () => {
   return (
     <ScrollView>
-      {/* <ToggleMode /> */}
-      {/* <Style02 /> */}
       <Main />
     </ScrollView>
   )
