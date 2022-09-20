1e73c6d24f21ebf1fdc4a9e2d86959b47cc4c327
salutJuillet <salut.juilllet@gmail.com>
2022-09-20 17:02:02 +0900

    drawer 페이지 생성

---
+++ components/Collections.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Collections = () => {
+  return (
+    <View>
+      <Text>Collections</Text>
+    </View>
+  )
+}
+
+export default Collections
---
+++ components/StoreLocation.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const StoreLocation = () => {
+  return (
+    <View>
+      <Text>StoreLocation</Text>
+    </View>
+  )
+}
+
+export default StoreLocation
